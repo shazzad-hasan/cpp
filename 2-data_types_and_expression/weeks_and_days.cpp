c9b9ee4646309be788ec691f08621a5ef1262fe2
Shazzad Hasan <shazzadraihan@gmail.com>
2023-05-03 12:52:37 +0600

    updated

---
+++ 2-data_types_and_expression/weeks_and_days.cpp
@@ -0,0 +1,24 @@
+/* Write a program that reads from the user the number of days
+they traveled. The program will then print their traveling time
+in the format of full weeks and additional days.*/
+
+#include <iostream>
+using namespace std;
+
+const int DAYS_IN_A_WEEK = 7;
+
+int main()
+{
+  int daysTraveled;
+  int fullWeeks, remainingDays;
+
+  cout<<"Please enter number of days you traveled: "<<endl;
+  cin>>daysTraveled;
+
+  fullWeeks = daysTraveled / DAYS_IN_A_WEEK;
+  remainingDays = daysTraveled % DAYS_IN_A_WEEK;
+
+  cout<<daysTraveled<<" days are "<<fullWeeks<<" weeks and "<<remainingDays<<" days."<<endl;
+
+  return 0;
+}

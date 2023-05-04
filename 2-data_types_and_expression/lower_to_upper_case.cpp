4ff334965aca1f1193625e29265dde485f4d29f3
Shazzad Hasan <shazzadraihan@gmail.com>
2023-05-05 02:07:56 +0600

    convert to upper case letter

---
+++ 2-data_types_and_expression/lower_to_upper_case.cpp
@@ -0,0 +1,20 @@
+/* Write a program that reads from the user a lower case letter,
+and prints it's corresponding upper case letter. */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  char lowerCaseLetter, upperCaseLetter;
+  int offset;
+
+  cout<<"Please enter a lower case letter: "<<endl;
+  cin>>lowerCaseLetter;
+
+  offset = (int) (lowerCaseLetter - 'a');
+  upperCaseLetter = (char) ('A' + offset);
+
+  cout<<"The upper case of "<<lowerCaseLetter<<" is "<<upperCaseLetter<<endl;
+
+  return 0;
+}

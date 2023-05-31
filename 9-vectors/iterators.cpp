ecbfbe2dd6c4a59779975620a585611fa4678012
Shazzad Hasan <shazzadraihan@gmail.com>
2023-06-01 02:33:15 +0600

    iterators

--- 9-vectors/iterators.cpp
+++ 9-vectors/iterators.cpp
@@ -7,25 +7,43 @@ using namespace std;
 
 int main(){
 
-  vector<int> numbers;
+  vector<int> v;
 
   // fill out vector numbers with numbers from 1 to 10
   for (int i=1; i<=10; i++)
-    numbers.push_back(i);
+    v.push_back(i);
 
   // print out the elements of vector numbers
 
-  for (int j=0; j<numbers.size(); j++)
-    cout<<numbers[j]<<endl;
+  for (int i=0; i<v.size(); i++)
+    cout<<v[i]<<" ";
 
-  for (int num:numbers)
-    cout<<num<<endl;
+  cout<<"\nusing for-each / range-for loop: ";
+  for (int e:v)
+    cout<<e<<" ";
+
+  cout<<"\nusing for-each / range-for loop: ";
+  for (auto e:v)
+    cout<<e<<" ";
 
   // using iterators
-  for (auto i:numbers.begin)
 
-  
+  cout <<"\nusing begin() and end() functions: ";
+  for (auto e = v.begin(); e != v.end(); e++)
+    // access element using dereference operator
+    cout<<*e<<" ";
 
+  cout <<"\nusing cbegin() and cend() functions: ";
+  for (auto e = v.cbegin(); e != v.cend(); e++)
+    cout<<*e<<" ";
+
+  cout <<"\nusing rbegin() and rend() functions: ";
+  for (auto e = v.rbegin(); e != v.rend(); e++)
+    cout<<*e<<" ";
+
+  cout <<"\nusing crbegin() and crend() functions: ";
+  for (auto e = v.crbegin(); e != v.crend(); e++)
+    cout<<*e<<" ";
 
   return 0;
 }
